e63ec635c0502db6a53d72ae31ee5182040df25d
steph <reyrel.stephane@sfr.fr>
2018-09-06 09:12:24 +0200

    création du contrôleur et des vues de base avec bootstrap + classe d'import Command

---
+++ src/Command/ImportDataCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ImportDataCommand extends Command
+{
+
+    protected function configure()
+    {
+
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        //code d'import des données issues de l'api de themovieDB
+
+    }
+}
