cd36ac614314a86a4197ea28911ed10b42b62d59
steph <reyrel.stephane@sfr.fr>
2018-09-12 10:37:15 +0200

    modif readme and réarrangement

--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-ébauche d'un projet web de gestion de films type TMDB (TheMovieDB)
+ébauche d'un projet web de gestion de films type TMDB (TheMovieDB),avec Symfony4
--- src/Controller/MovieStoreController.php
+++ src/Controller/MovieStoreController.php
@@ -4,18 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Film;
 use App\Entity\Personne;
-use App\Entity\Fonction;
 use App\Repository\FilmRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieStoreController extends Controller
 {
+
     /**
      * Affiche la liste des films en page d'accueil
      *
      * @Route("/", name="accueil")
-     * 
      */
     public function home(FilmRepository $repo)
     {
@@ -29,40 +28,33 @@ class MovieStoreController extends Controller
      * Affiche les détails d'un film
      *
      * @Route("/film/{id}", name="film_show")
-     *  
      */
-
     public function showFilm($id)
     {
-   
-    $film = $this->getDoctrine()->getRepository(Film::class)->find($id);
-    $personnes = $film->getPersonnes(); 
- 
-   
-      return $this->render('movie_store/show_film.html.twig', [
+        $film = $this->getDoctrine()->getRepository(Film::class)->find($id);
+        $personnes = $film->getPersonnes();
+
+        return $this->render('movie_store/show_film.html.twig', [
             'film' => $film,
-            'personnes'=> $personnes,
- 
+            'personnes' => $personnes,
+
         ]);
     }
     /**
      * Affiche les détails d'une personne
      *
      * @Route("/personne/{id}", name="person_show")
-     * 
      */
-
     public function showPerson($id)
     {
- 
-        $personne= $this->getDoctrine()->getRepository(Personne::class)->find($id);
+        $personne = $this->getDoctrine()->getRepository(Personne::class)->find($id);
         $films = $personne->getFilms();
         $fonctions = $personne->getFonctions();
 
-      return $this->render('movie_store/show_person.html.twig', [
+        return $this->render('movie_store/show_person.html.twig', [
             'films' => $films,
             'personne' => $personne,
-            'fonctions' => $fonctions
+            'fonctions' => $fonctions,
         ]);
     }
 }
